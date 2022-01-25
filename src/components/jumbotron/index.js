dc607ac354bc55500100a416ea8563d7375722ff
Evgene Popolamov <popolamov908@gmail.com>
2022-01-25 21:11:33 +0300

    feat: add glodal styles

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Container, Inner } from './styles/jumbotron';
+import { Container, Inner, Item, Pane, Title, SubTitle, Image } from './styles/jumbotron';
 
 const Jumbotron = ({children, direction = 'row', ...restProps}) => {
   return (
-      <Inner direction={direction}>
-          <p>Hello inner</p>
-      </Inner>
+      <Item direction={direction}>
+        <Inner>
+          {children}
+        </Inner>
+      </Item>
   );
 };
+
+export default Jumbotron;
 
 Jumbotron.Container = function JumbotronContainer({children, ...restProps}) {
     return <Container { ...restProps }>{ children }</Container>
 }
 
-export default Jumbotron;
+Jumbotron.Pane = function JumbotronPane({children, ...restProps}) {
+    return <Pane { ...restProps }>{ children }</Pane>
+}
+
+Jumbotron.Title = function JumbotronTitle({children, ...restProps}) {
+    return <Title { ...restProps }>{ children }</Title>
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({children, ...restProps}) {
+    return <SubTitle { ...restProps }>{ children }</SubTitle>
+}
+
+Jumbotron.Image = function JumbotronImage({...restProps}) {
+    return <Image { ...restProps } />
+}
