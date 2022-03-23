20a9feb7325ec8c5e8528cf158f3ab4b5f8708a5
Evgene Popolamov <popolamov908@gmail.com>
2022-03-23 20:19:05 +0300

    feat: add styles for accordion

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,3 +1,9 @@
+import React, { useState, createContext, useContext } from 'react'
+import { Container, Inner, Title, Frame, Item, Header, Body } from './styles/accordion'
+
+
+const ToggleContext = createContext();
+
 const Accordion = ({children, ...restProps}) => {
     return(
         <Container {...restProps}>
@@ -14,4 +20,21 @@ Accordion.Title = function AccordionTitle({children, ...restProps}) {
 
 Accordion.Frame = function AccordionFrame({children, ...restProps}) {
     return <Frame {...restProps}>{children}</Frame>
+}
+
+Accordion.Item = function AccordionItem({children, ...restProps}) {
+    const [toggleShow, setToggleShow] = useState(false)
+    return  <ToggleContext.Provider value={{toggleShow, setToggleShow}}>
+                <Item {...restProps}>{children}</Item>
+            </ToggleContext.Provider> 
+}
+
+Accordion.Header = function AccordionHeader({children, ...restProps}) {
+    const {toggleShow, setToggleShow} = useContext(ToggleContext)
+    return <Header onClick={()=> setToggleShow(toggleShow => !toggleShow)} {...restProps}>{children}</Header>
+}
+
+Accordion.Body = function AccordionBody({children, ...restProps}) {
+    const { toggleShow } = useContext(ToggleContext)
+    return toggleShow ? <Body  {...restProps}>{children}</Body> : null
 }
